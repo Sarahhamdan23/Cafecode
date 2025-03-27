611ae042e818287af2364d48e53e63fed7b8b648
Sarah Hamdan <sarahwbas@gmail.com>
2025-03-27 15:14:54 -0500

    Updates

---
+++ precache-manifest.3368e8902217a265ffbf64d2576ba7a9.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a45f84266a2433fe9fcbdbe13cb4f59",
+    "url": "/cafecode/index.html"
+  },
+  {
+    "revision": "e6edc71aa8364909b245",
+    "url": "/cafecode/static/css/main.5b9666ca.chunk.css"
+  },
+  {
+    "revision": "08204076f7774b71e07d",
+    "url": "/cafecode/static/js/2.5b5cb4f4.chunk.js"
+  },
+  {
+    "revision": "db4976e2c3fc371be79e",
+    "url": "/cafecode/static/js/3.1af95c37.chunk.js"
+  },
+  {
+    "revision": "e6edc71aa8364909b245",
+    "url": "/cafecode/static/js/main.0cdd137b.chunk.js"
+  },
+  {
+    "revision": "fe8ba6d2c559b98979d4",
+    "url": "/cafecode/static/js/runtime~main.6ec20091.js"
+  },
+  {
+    "revision": "a9b737bf91ae229a76a95c7d02a3610c",
+    "url": "/cafecode/static/media/CybersecurityCertif.a9b737bf.png"
+  },
+  {
+    "revision": "501b7f348ca63dcc70193e66d8ffdea5",
+    "url": "/cafecode/static/media/Emailme.501b7f34.png"
+  },
+  {
+    "revision": "1108618d651101d608360d604a218255",
+    "url": "/cafecode/static/media/GitHubIcon.1108618d.png"
+  },
+  {
+    "revision": "e66a5ec6182509bd490b89597441e0dd",
+    "url": "/cafecode/static/media/LinkedinIcon.e66a5ec6.png"
+  },
+  {
+    "revision": "1a5ea312bf130d6b7f531261ce7289d8",
+    "url": "/cafecode/static/media/MITCertif.1a5ea312.png"
+  },
+  {
+    "revision": "02d995fbc99a470a23212089aa32f5bb",
+    "url": "/cafecode/static/media/MyPic.02d995fb.png"
+  },
+  {
+    "revision": "bbee9601eca294de6c05c37dda4dd92e",
+    "url": "/cafecode/static/media/MyProfilePic2.bbee9601.png"
+  },
+  {
+    "revision": "5f405cb4af5b4d30180ffa6cbff69dba",
+    "url": "/cafecode/static/media/MyProject.5f405cb4.png"
+  }
+]);
